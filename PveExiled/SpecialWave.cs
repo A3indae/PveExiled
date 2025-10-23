d29ecb76a8845599881c3d4ce52e0a752b314424
A3indae <128219284+A3indae@users.noreply.github.com>
2025-10-24 00:32:05 +0900

    Delete PveExiled directory

--- PveExiled/SpecialWave.cs
+++
@@ -1,8 +0,0 @@
-﻿public abstract class SpecialWave
-{
-    public bool Ended = false;
-    public abstract string SpecialWaveName { get; }
-    public abstract string SoundtrackName { get; }
-    public abstract void Enable(RoundHandler roundHandler, WaveConfig waveConfig, WaveConfig.WaveInfo waveInfo);
-    public abstract void Disable();
-}
